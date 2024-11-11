03c5264b00430f80dd21765affb23f0885ff8ddf
evyariani18 <evyariani180105@gmail.com>
2024-11-11 09:50:00 +0800

    Deskripsi perubahan pada routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TransaksiController; // Import TransaksiController
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\LoginController; // Import LoginController
+use App\Http\Controllers\Dashboard2Controller; // Import Dashboard2Controller untuk dashboard tambahan
 
 // Rute halaman utama
 Route::get('/', function () {
@@ -20,6 +21,12 @@ Route::get('/', function () {
 Route::get('/lihat-barang', function () {
     return view('tambah_barang'); // Sesuaikan jika diperlukan view yang berbeda
 });
+
+// Rute untuk halaman dashboard utama
+Route::get('/dashboard', [DashboardController::class, 'home'])->name('dashboard');
+
+// Rute untuk halaman dashboard tambahan
+Route::get('/dashboard2', [Dashboard2Controller::class, 'home'])->name('dashboard2.home');
 
 // Rute resource untuk brands
 Route::resource('brands', BrandController::class);
@@ -38,4 +45,3 @@ Route::resource('transaksi', TransaksiController::class); // Rute resource untuk
 
 // Rute untuk login dan user
 Route::resource('users', UserController::class);
-
